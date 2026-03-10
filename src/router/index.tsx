00f703e68b3f56b59fe2e632e7622df208b6a242
jingxuan <thichakorn@abbot.tech>
2026-03-10 12:01:06 +0700

    -

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,7 @@
 import { createBrowserRouter, redirect } from "react-router";
 import MainLayout from "../layouts/main";
 import WelcomePage from "../modules/welcome/page";
-<<<<<<< HEAD
-import RadioPage from "../modules/radioGroup/page";
-=======
 import RadioGroup from "../modules/radioGroup/page";
->>>>>>> 6a2894af16a001acb3f208ab61d80f03224f5b21
 
 export const router = createBrowserRouter([
   {
@@ -21,13 +17,8 @@ export const router = createBrowserRouter([
         Component: WelcomePage,
       },
       {
-<<<<<<< HEAD
-        path: "/radio",
-        Component: RadioPage,
-=======
         path: "radio-group",
         Component: RadioGroup,
->>>>>>> 6a2894af16a001acb3f208ab61d80f03224f5b21
       },
     ],
   },
